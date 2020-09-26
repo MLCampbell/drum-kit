839bd8f8b55e359fd9bbab74e5dc017b1a252b20
MLCampbell <marcus.l.campbell@gmail.com>
2020-09-26 17:53:38 +1200

    Added event listeners for button & key press

---
+++ index.js
@@ -0,0 +1,57 @@
+for (i = 0; i < document.querySelectorAll('.drum').length; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+        var buttonInnerHTML = this.innerHTML;
+        makeSound(buttonInnerHTML);
+    });
+}
+
+document.addEventListener("keydown", function(press) {
+    makeSound(press.key)
+});
+
+function makeSound(keystroke) {
+    switch (keystroke) {
+        case "w":
+            var kick = new Audio("sounds/kick-bass.mp3");
+            kick.play();
+            break;
+
+        case "a":
+            var snare = new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+
+        case "i":
+            var hiHat = new Audio("sounds/hi-hat.mp3");
+            hiHat.play();
+            break;
+
+        case "s":
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+
+        case "d":
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+
+        case "j":
+            var tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+
+        case "k":
+            var tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+
+        case "l":
+            var crash = new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+
+        default:
+            break;
+    }
+}
